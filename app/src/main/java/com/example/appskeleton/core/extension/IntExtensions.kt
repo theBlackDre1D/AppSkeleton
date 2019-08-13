51f873a239237f05d919ff1c4a8d1340273089c1
Michal Gaborik <michal.gaborik.77@gmail.com>
2019-08-13 15:26:14 +0200

    added - useful extensions from another project

--- app/src/main/java/com/example/appskeleton/core/extension/IntExtensions.kt
+++ app/src/main/java/com/example/appskeleton/core/extension/IntExtensions.kt
@@ -1,5 +1,26 @@
 package com.example.appskeleton.core.extension
 
 import android.content.Context
+import android.content.res.Resources
+import androidx.core.content.ContextCompat
+import com.example.appskeleton.core.App
 
 fun Int.colorFromResources(context: Context) = context.resources.getColor(this)
+
+fun Int.stringFromResources(): String = if (this < 1) "" else
+    App.currentActivity.get()!!.applicationContext.getString(this)
+
+fun Int.stringFromResources(vararg formats: Any): String = if (this < 1) "" else
+    App.currentActivity.get()!!.applicationContext.getString(this, *formats)
+
+fun Int.stringQuantityFromResources(quantity: Int, vararg formats: Any): String = if (this < 1) "" else
+    App.currentActivity.get()!!.applicationContext.resources.getQuantityString(this, quantity, *formats)
+
+fun Int.colorFromResources(): Int =
+    ContextCompat.getColor(App.currentActivity.get()!!.applicationContext, this)
+
+val Int.dp: Int
+    get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+
+val Int.px: Int
+    get() = (this * Resources.getSystem().displayMetrics.density).toInt()
